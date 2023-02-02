76181222ecbc14d06c7c8019d58b4362a1fdd4c8
Oliver8985 <109213155+Oliver8985@users.noreply.github.com>
2023-02-02 13:48:33 +0000

    Update models.py

--- website/models.py
+++ website/models.py
@@ -7,8 +7,8 @@ class Timetable(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.String(10000))
     date = db.Column(db.DateTime(timezone=True), default=func.now())
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id')) # user in lower case as a reference from class user because of sql convention
+# create a one to many relationship
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
